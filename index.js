1e80eced454c7085a2e9ad2dd94f861d9183c446
Gustavout22 <gustavougartetorres@gmail.com>
2023-05-27 15:12:12 -0500

    copy changes

---
+++ index.js
@@ -0,0 +1,84 @@
+let notes = [
+  {title: "note 1", content: "hellowwwwwwww world"},
+  {title: "note 2", content: "the lide is so hard"},
+  {title: "note 3", content: "I only know that I know nothing"},
+  {title: "note 4", content: "I didnt do my cv yet"},
+  {title: "note 5", content: "I have to do the assigment"},
+  {title: "note 6", content: "I do this project"}
+]
+
+//Delete Note 
+function deleteNote(note) {
+  const index = notes.indexOf(note)
+  notes.splice(index,1)
+  console.log(notes)
+}
+
+//Create Element
+function createNoteEl(note) {
+  const li = document.createElement("li");
+  li.classList.add("note");
+
+  const noteDetails = document.createElement("div");
+  noteDetails.classList.add("note-details");
+
+  const title = document.createElement("h3");
+  title.textContent = note.title;
+
+  const content = document.createElement("p");
+  content.textContent = note.content;
+
+  const noteActions = document.createElement("div");
+  noteActions.classList.add("note-actions");
+
+  const colorButton = document.createElement("button");
+  colorButton.classList.add("color-button");
+  colorButton.textContent = "COLOR";
+
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("delete-button");
+  deleteButton.textContent = "DELETE";
+
+  const deleteIcon = document.createElement("img");
+  deleteIcon.src = "assets/icons/delete.svg";
+  deleteIcon.alt = "Delete note";
+
+  const colorIcon = document.createElement("img");
+  colorIcon.src = "assets/icons/color.svg";
+  colorIcon.alt = "Change color";
+
+  li.appendChild(noteDetails);
+  li.appendChild(noteActions);
+  noteDetails.appendChild(title);
+  noteDetails.appendChild(content);
+  noteActions.appendChild(colorButton);
+  noteActions.appendChild(deleteButton);
+  deleteButton.appendChild(deleteIcon);
+  colorButton.appendChild(colorIcon);
+
+  deleteButton.addEventListener("click", (event) => {
+     deleteNote(note)
+     renderNotes(notes)
+  })
+  return li;
+
+}
+
+//MOSTRAR LAS NOTAS
+function renderNotes(notes) {
+  const notesList = document.querySelector(".notes");
+  notesList.innerHTML = "";
+
+  notes.forEach((note) => {
+    const noteEl = createNoteEl(note);
+    notesList.appendChild(noteEl);
+  });
+}
+
+renderNotes(notes);
+
+
+
+// function addNote(noteID) {
+
+// }
