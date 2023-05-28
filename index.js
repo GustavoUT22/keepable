256b1b1fdf65668879f67c62cddac4a7d1e1ff9f
Gustavout22 <gustavougartetorres@gmail.com>
2023-05-27 19:42:07 -0500

    add: storage.js

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-let initialNotes = [
-  {title: "note 1", content: "hellowwwwwwww world"},
-  {title: "note 2", content: "the lide is so hard"},
-  {title: "note 3", content: "I only know that I know nothing"},
-  {title: "note 4", content: "I didnt do my cv yet"},
-  {title: "note 5", content: "I have to do the assigment"},
-  {title: "note 6", content: "I do this project"}
-]
-
-const notesFromStorage = JSON.parse(localStorage.getItem("notes"))
-const notes = notesFromStorage || initialNotes
 
 //Delete Note 
 function deleteNote(note) {
---
+++ new-note.js
@@ -0,0 +1,14 @@
+const form = document.querySelector(".note-form")
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    // const title = event.target.elements.title.value
+    // const content = event.target.elements.content.value
+    const { title, content} = event.target.elements
+    const newNote = { 
+                    title: title.value,
+                    content: content.value
+    };
+    notes.push(newNote)
+    
+});
+
